759e67391a7f2641c6a0a160d1663ab7ad49468f
Jimin Huh <jiminhuh95@gmail.com>
2021-02-20 10:22:33 -0600

    finished project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+    
+    func playSound(key:String) {
+        guard let url = Bundle.main.url(forResource: key, withExtension: "wav") else { return print("No Sound") }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
-
-
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let musicalKeyPressed =  sender.currentTitle!
+        playSound(key: musicalKeyPressed)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        //Bring's sender's opacity back up to fully opaque.
+        sender.alpha = 1.0
+        }
 
+
+    }
+    
 }
-
